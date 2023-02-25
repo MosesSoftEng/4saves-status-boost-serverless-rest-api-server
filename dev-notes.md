098f4ec964c1224d4a75bf331aa8bc12434f3b52
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-02-25 19:33:12 +0300

    Set up serverless configurations

---
+++ dev-notes.md
@@ -0,0 +1,17 @@
+```bash
+# Create a new serverless project in current directory.
+sls create --template aws-nodejs-typescript --path .
+
+# Deploy to AWS
+sls deploy --aws-profile completecoding.io-serverless
+
+# Create a users feature.
+mkdir -p src/functions/users
+cd src/functions/users
+touch index.ts handler.ts schema.ts service.ts repository.ts model.ts mock.json
+
+# Install aws sdk as a development dependecy.
+npm install aws-sdk --save-dev
+
+
+```
--- serverless.ts
+++ serverless.ts
@@ -1,38 +1,103 @@
-import type { AWS } from '@serverless/typescript';
+import type {AWS} from '@serverless/typescript';
 
 import hello from '@functions/hello';
+import createUser from '@functions/users';
 
 const serverlessConfiguration: AWS = {
-  service: 'server-4saves-status-boost',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-    },
-  },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
-    },
-  },
+	service: 'four-saves',
+	frameworkVersion: '3',
+	plugins: [
+		'serverless-esbuild',
+		'serverless-offline',
+		// Plugin to allow Custom Roles for each Lambda Function.
+		'serverless-iam-roles-per-function',
+	],
+	provider: {
+		// AWS profile to use.
+		profile: 'completecoding.io-serverless',
+		stage: 'dev',
+		name: 'aws',
+		runtime: 'nodejs14.x',
+		region: 'us-east-1',
+		apiGateway: {
+			minimumCompressionSize: 1024,
+			shouldStartNameWithService: true,
+		},
+
+		// Environment variables.
+		environment: {
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+			NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+			TABLE_USERS: '${self:service}-users-${self:provider.stage}',
+			TABLE_SAVES: '${self:service}-saves-${self:provider.stage}',
+		},
+	},
+
+	/*
+	 * Lambda Functions.
+	 */
+	// Import the function via paths.
+	functions: {hello, createUser},
+	package: {individually: true},
+	custom: {
+		esbuild: {
+			bundle: true,
+			minify: false,
+			sourcemap: true,
+			exclude: ['aws-sdk'],
+			target: 'node14',
+			define: {'require.resolve': undefined},
+			platform: 'node',
+			concurrency: 10,
+		},
+	},
+
+	/*
+	 * Resources
+	 */
+	resources: {
+		Resources: {
+			// DynamoDB tables.
+			UsersTable: {
+				Type: 'AWS::DynamoDB::Table',
+				Properties: {
+					TableName: '${self:provider.environment.TABLE_USERS}',
+					AttributeDefinitions: [
+						{
+							AttributeName: 'phone',
+							AttributeType: 'S',
+						}
+					],
+					KeySchema: [
+						{
+							AttributeName: 'phone',
+							KeyType: 'HASH',
+						}
+					],
+					BillingMode: 'PAY_PER_REQUEST'
+				},
+			},
+			SavesTable: {
+				Type: 'AWS::DynamoDB::Table',
+				Properties: {
+					TableName: '${self:provider.environment.TABLE_SAVES}',
+					AttributeDefinitions: [
+						{
+							AttributeName: 'userId',
+							AttributeType: 'S',
+						}
+					],
+					KeySchema: [
+						{
+							AttributeName: 'userId',
+							KeyType: 'HASH',
+						}
+					],
+					BillingMode: 'PAY_PER_REQUEST'
+				},
+			},
+		}
+	}
 };
 
 module.exports = serverlessConfiguration;
