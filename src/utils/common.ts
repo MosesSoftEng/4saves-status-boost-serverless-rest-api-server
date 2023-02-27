149950f19822068a1236a27c78781c7cb3496cac
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-02-27 15:39:47 +0300

    Create common.ts

---
+++ src/utils/common.ts
@@ -0,0 +1,11 @@
+export function generateCode(length: number): string {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+	let result = ' ';
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+
+	return result;
+}
