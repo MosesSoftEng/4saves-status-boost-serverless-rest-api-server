995f11aee9ace7b0e285d6512c54e6c8acb7b910
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-03-06 09:44:37 +0300

    Remove unused functions

--- src/functions/user/code/repository.ts
+++ src/functions/user/code/repository.ts
@@ -5,24 +5,3 @@
 import * as AWS from 'aws-sdk';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
-
-export async function getUserByPhone(userPhone: string): Promise<User> {
-	const result = await docClient.get({
-		TableName: process.env.TABLE_USERS,
-		Key: {
-			phone: userPhone,
-		},
-	}).promise();
-
-	return result.Item as User;
-}
-
-export async function createUserCode(phone: string): Promise<string> {
-	
-	await docClient.put({
-		TableName: process.env.TABLE_USERS,
-		Item: user,
-	}).promise();
-
-	return user;
-}
